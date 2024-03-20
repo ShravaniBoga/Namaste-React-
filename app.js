f128ef8cfc8b5bb02c3ae100eaf41890e2a99315
BOGA SHRAVANI <shravaniboga07@gmail.com>
2024-03-20 15:06:21 +0530

    episode 1

---
+++ app.js
@@ -0,0 +1,20 @@
+// const heading = React.createElement("h1", {}, "hello world");
+// const root = ReactDOM.createRoot(document.getElementsById("root"));
+// root.render(heading);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "hello world baby"
+// );
+
+// // Get the root DOM element
+// // const root = document.getElementById("root");
+
+// // Create a React root and render the element
+// const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
+// reactRoot.render(heading);
+
+// Namaste React Episode 1🚀
+
+let parent = React.createElement(h1);
